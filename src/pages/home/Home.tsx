ac39a58231b591ca16ff4f80de917b2e3f8fad60
Matvey <matvejaksentev80@gmail.com>
2024-08-03 02:38:32 +0700

    A-13

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,10 +3,15 @@ import energyIcon from '@/assets/icons/energy.svg';
 import bgImg from '@/assets/images/bg-home.png';
 import goldCoin from '@/assets/images/gold-coin.png';
 import trumpCharacter from '@/assets/images/trump/character_model_01.svg';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useState } from 'react';
 import Profile from '../../components/shared/Profile/Profile';
 import styles from './Home.module.css';
 
 export default function HomePage() {
+	const [clientX, setClientX] = useState(0);
+	const [clientY, setClientY] = useState(0);
+
 	return (
 		<div className={styles.home}>
 			<img src={bgImg} alt="" className={styles.bgImage} />
@@ -15,7 +20,26 @@ export default function HomePage() {
 				<img src={coinIcon} alt="" className={styles.coins_img} />
 				<div className={styles.coins_text}>120.50</div>
 			</div>
-			<div className={styles.click}>
+			<div
+				className={styles.click}
+				onClick={event => {
+					setClientX(event.nativeEvent.layerX);
+					setClientY(event.nativeEvent.layerY);
+				}}
+			>
+				<AnimatePresence mode="wait">
+					<motion.div
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						className={styles.tap}
+						style={{
+							position: 'absolute',
+							transform: `translate(${clientX}px, ${clientY}px)`,
+						}}
+					>
+						+1
+					</motion.div>
+				</AnimatePresence>
 				<img src={trumpCharacter} alt="" className={styles.click_character} />
 				<img
 					src={goldCoin}
--- src/pages/improves/Product/AnimationCoins/AnimationCoins.tsx
+++ src/pages/improves/Product/AnimationCoins/AnimationCoins.tsx
@@ -1,19 +1,133 @@
-import coinsImg from '@/assets/images/improves/animations.png';
+import coinIcon from '@/assets/icons/coin-icon.svg';
 import { AnimatePresence, motion } from 'framer-motion';
 import styles from './AnimationCoins.module.css';
 
 const AnimationCoins = () => {
 	return (
 		<AnimatePresence mode="wait">
-			<motion.div
-				initial={{ y: '-60%', height: 0, opacity: 0 }}
-				animate={{ y: '-68%', height: '314px', opacity: 1 }}
-				exit={{ y: '-60%', height: 0, opacity: 0 }}
-				transition={{ bounce: 3000 }}
-				className={styles.coins}
-			>
-				<img src={coinsImg} alt="" />
-			</motion.div>
+			<div className={styles.coins}>
+				<motion.div
+					initial={{ y: 0, x: 0 }}
+					animate={{ y: -3, x: 0 }}
+					transition={{
+						x: { type: 'spring', stiffness: 300, damping: 30 },
+						opacity: { duration: 0.2 },
+					}}
+					className={styles.img}
+				>
+					<img src={coinIcon} alt="" />
+				</motion.div>
+				<motion.div
+					initial={{ y: 0, x: 40 }}
+					animate={{ y: -13, x: 40 }}
+					transition={{
+						x: { type: 'spring', stiffness: 300, damping: 30 },
+						opacity: { duration: 0.2 },
+					}}
+					className={styles.img}
+				>
+					<img src={coinIcon} alt="" />
+				</motion.div>
+				<motion.div
+					initial={{ y: 0, x: 80 }}
+					animate={{ y: -7, x: 80 }}
+					transition={{
+						x: { type: 'spring', stiffness: 300, damping: 30 },
+						opacity: { duration: 0.2 },
+					}}
+					className={styles.img}
+				>
+					<img src={coinIcon} alt="" />
+				</motion.div>
+				<motion.div
+					initial={{ y: 0, x: 120 }}
+					animate={{ y: -2, x: 120 }}
+					transition={{
+						x: { type: 'spring', stiffness: 300, damping: 30 },
+						opacity: { duration: 0.2 },
+					}}
+					className={styles.img}
+				>
+					<img src={coinIcon} alt="" />
+				</motion.div>
+				<motion.div
+					initial={{ y: 0, x: 0, opacity: 1 }}
+					animate={{ y: -40, x: 0 }}
+					transition={{
+						x: { type: 'spring', stiffness: 300, damping: 30 },
+						opacity: { duration: 0.2 },
+					}}
+					className={styles.img}
+				>
+					<img src={coinIcon} alt="" />
+				</motion.div>
+				<motion.div
+					initial={{ y: 0, x: 40, opacity: 1 }}
+					animate={{ y: -60, x: 40, opacity: 0.8 }}
+					transition={{
+						x: { type: 'spring', stiffness: 300, damping: 30 },
+						opacity: { duration: 0.2 },
+					}}
+					className={styles.img}
+				>
+					<img src={coinIcon} alt="" />
+				</motion.div>
+				<motion.div
+					initial={{ y: 0, x: 0, opacity: 1 }}
+					animate={{ y: -45, x: 75 }}
+					transition={{
+						x: { type: 'spring', stiffness: 300, damping: 30 },
+						opacity: { duration: 0.2 },
+					}}
+					className={styles.img}
+				>
+					<img src={coinIcon} alt="" />
+				</motion.div>
+				<motion.div
+					initial={{ y: 0, x: 120, opacity: 1 }}
+					animate={{ y: -54, x: 120, opacity: 0.8 }}
+					transition={{
+						x: { type: 'spring', stiffness: 300, damping: 30 },
+						opacity: { duration: 0.2 },
+					}}
+					className={styles.img}
+				>
+					<img src={coinIcon} alt="" />
+				</motion.div>
+				<motion.div
+					initial={{ y: 0, x: 0, opacity: 1 }}
+					animate={{ y: -85, x: 5, opacity: 0.5 }}
+					transition={{
+						x: { type: 'spring', stiffness: 300, damping: 30 },
+						opacity: { duration: 0.2 },
+					}}
+					className={styles.img}
+				>
+					<img src={coinIcon} alt="" />
+				</motion.div>
+				<motion.div
+					initial={{ y: 0, x: 45, opacity: 1 }}
+					animate={{ y: -100, x: 54, opacity: 0.5 }}
+					transition={{
+						x: { type: 'spring', stiffness: 300, damping: 30 },
+						opacity: { duration: 0.2 },
+					}}
+					className={styles.img}
+				>
+					<img src={coinIcon} alt="" />
+				</motion.div>
+				<motion.div
+					initial={{ y: 0, x: 32, opacity: 1 }}
+					animate={{ y: -85, x: 75, opacity: 0.5 }}
+					transition={{
+						x: { type: 'spring', stiffness: 300, damping: 30 },
+						opacity: { duration: 0.2 },
+					}}
+					className={styles.img}
+				>
+					<img src={coinIcon} alt="" />
+				</motion.div>
+			</div>
 		</AnimatePresence>
 	);
 };
