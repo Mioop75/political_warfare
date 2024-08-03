e52ecc66f82139e3fa3a49a593ba131a3f6ac015
Matvey <matvejaksentev80@gmail.com>
2024-08-03 17:06:59 +0700

    A-20

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { useEffect, useState } from 'react';
 import Router from './Router';
+import Loading from './components/shared/Loading/Loading';
 
 function App() {
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => setIsLoading(false), 5000);
+	}, []);
+
+	if (isLoading) return <Loading />;
+
 	return (
 		<>
 			<Router />
--- src/components/shared/Loading/Loading.tsx
+++ src/components/shared/Loading/Loading.tsx
@@ -1,7 +1,31 @@
+import logo from '@/assets/icons/logo.svg';
+import { AnimatePresence, motion } from 'framer-motion';
+import Text from '../../ui/Text/Text';
 import styles from './Loading.module.css';
 
 const Loading = () => {
-	return <div className={styles.loading}>Loading</div>;
+	return (
+		<div className={styles.loading}>
+			<img src={logo} alt="" className={styles.logo} />
+			<div className={styles.progress}>
+				<Text className={styles.progress_info}>Загрузка</Text>
+				<div className={styles.bar}>
+					<div className={styles.bar_wrapper}>
+						<div className={styles.box}>
+							<AnimatePresence mode="sync">
+								<motion.div
+									initial={{ width: '0%' }}
+									animate={{ width: '100%' }}
+									transition={{ duration: 5 }}
+									className={styles.line}
+								></motion.div>
+							</AnimatePresence>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Loading;
